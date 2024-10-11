80c983f67d836cad1bbaba8a361c2cd9a279c804
Jai Verma <vermajai2004@gmail.com>
2024-10-11 11:30:32 +0530

    updated

--- utils/image_gis.py
+++ utils/image_gis.py
@@ -3,16 +3,16 @@ from config import GIS_API_KEY, CX_ID
 
 gis = GoogleImagesSearch(GIS_API_KEY, CX_ID)
 
-query = 'IPL'
+query = 'player'
 fileType = 'jpg'
 
 _search_params = {
     'q': query,
     'num': 1,
-    'imgsize':'huge',
+    'imgType': 'clipart',
     'fileType': fileType,
 }
 try:
-    gis.search(search_params=_search_params, path_to_dir = "..\\static\\others", custom_image_name=f"{query}.{fileType}")
+    gis.search(search_params=_search_params, path_to_dir = "..\\static\\icons", custom_image_name=f"{query}")
 except Exception as e:
     print(e)
