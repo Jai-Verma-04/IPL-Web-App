bb734970a67bd5cd98d0679114d57575e4b19bc3
Jai Verma <vermajai2004@gmail.com>
2024-10-17 12:01:27 +0530

    added reading from parquet files

--- utils/read_data.py
+++ utils/read_data.py
@@ -6,15 +6,14 @@ import os
 current_dir = os.getcwd()
 sys.path.append(os.path.join(current_dir))
 
-from config import MATCHES_PROCESSED_DATA, DELIVERIES_PROCESSED_DATA
-
+print(os.getcwd())
 
 def matches_data():
-    matches = pd.read_csv(MATCHES_PROCESSED_DATA)
+    matches = pd.read_parquet("data\\processed\\matches.parquet")
 
     return matches
 
 
 def deliveries_data():
-    deliveries = pd.read_csv(DELIVERIES_PROCESSED_DATA)
+    deliveries = pd.read_parquet("data\\processed\\deliveries.parquet")
     return deliveries
