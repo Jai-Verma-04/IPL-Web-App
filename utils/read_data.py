406cfa1dff31350895f190a291a3b3941e98dccf
Jai Verma <vermajai2004@gmail.com>
2024-10-17 21:16:11 +0530

    added documentation

--- utils/read_data.py
+++ utils/read_data.py
@@ -1,16 +1,38 @@
-import pandas as pd
+# -------------------------------------------------------------------------------------------------------
+# File Name: read_data.py
+# Author:   Jai Verma
+# Description: Reads the parquet data files from the data folder using pandas.
+# -------------------------------------------------------------------------------------------------------
 
+# Importing the required libraries
+import pandas as pd
 import sys
 import os
 
+# Setting the current working directory as system path
 current_dir = os.getcwd()
 sys.path.append(os.path.join(current_dir))
 
+
 def matches_data():
+    '''
+    Reads the Matches data from matches.parquet file.
+
+    Returns:
+    Dataframe: matches.parquet file as a dataframe.
+    '''
+
     matches = pd.read_parquet("data\\processed\\matches.parquet")
     return matches
 
 
 def deliveries_data():
+    '''
+    Reads the Deliveries data from deliveries.parquet file.
+    
+    Returns:
+    Dataframe: deliveries.parquet as a dataframe.
+    '''
+
     deliveries = pd.read_parquet("data\\processed\\deliveries.parquet")
     return deliveries
