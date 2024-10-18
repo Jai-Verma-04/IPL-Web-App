4098ce279e05e7ad683fdd5b779742cd91bc250d
Jai Verma <vermajai2004@gmail.com>
2024-10-18 13:31:35 +0530

    k

--- pages/IPL Summary.py
+++ pages/IPL Summary.py
@@ -9,7 +9,4 @@ sys.path.append(os.path.join(current_dir))
 
 from analysis import IPL_Summary
 
-try:
-    st.write(IPL_Summary.get_most_100s())
-except Exception as e:
-    st.write(Exception)
+st.write(IPL_Summary.get_most_100s())
