bd2cbbeff5c9b834ff25b6f9ac4a7dddd5078965
Jai Verma <vermajai2004@gmail.com>
2024-10-20 17:51:15 +0530

    Minor

--- pages/Player Wise Stats.py
+++ pages/Player Wise Stats.py
@@ -1,3 +1,23 @@
 import streamlit as st
 
 st.set_page_config(page_title="Player Wise Stats", page_icon="🤵", initial_sidebar_state="collapsed")
+
+import os
+import sys
+
+
+current_dir = os.getcwd()
+sys.path.append(os.path.join(current_dir))
+
+from analysis import player_analysis
+from utils import CONSTANTS
+
+player_name = st.selectbox("Choose a player (start typing the last name)", options = CONSTANTS.PLAYERS, index=None)
+
+
+st.write(f"Batting Average: {player_analysis.batting_average(player_name=player_name)}")
+st.write(f"Strike Rate: {player_analysis.strike_rate(player_name=player_name)}")
+st.write(f"powerplay runs: {player_analysis.runs_scored_in_powerplay(player_name=player_name)}")
+st.write(f"death over runs: {player_analysis.runs_scored_in_death_overs(player_name=player_name)}")
+st.write(f"Most Successful Batting Venue: {player_analysis.most_successful_venue(player_name=player_name)[0]}")
+st.write(f"Most Successful Bowling Venue: {player_analysis.most_successful_venue(player_name=player_name)[1]}")
--- pages/Team Wise Stats.py
+++ pages/Team Wise Stats.py
@@ -1,16 +1,13 @@
 import os
 import sys
-import time
+import streamlit as st
 
 
 current_dir = os.getcwd()
 sys.path.append(os.path.join(current_dir))
 
-import streamlit as st
-from analysis import team_analysis
 from utils import CONSTANTS
 
 team = st.selectbox("Choose a team", CONSTANTS.TEAMS)
 
 st.write("here is the graph")
-st.pyplot(team_analysis.graph(team))
