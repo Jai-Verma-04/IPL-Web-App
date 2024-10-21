0716719f71c316a5583da29b6a22f953d894a465
Jai Verma <vermajai2004@gmail.com>
2024-10-21 13:26:36 +0530

    added stadium names

--- utils/data.py
+++ utils/data.py
@@ -4,9 +4,6 @@
 # Description: A file for storing variables / data that is repeatedly used throughout the project.
 # -------------------------------------------------------------------------------------------------------
 
-import os
-import sys
-
 
 TEAMS = ['Royal Challengers Bangalore', 'Kings XI Punjab', 'Delhi Capitals',
        'Mumbai Indians', 'Kolkata Knight Riders', 'Rajasthan Royals',
@@ -15,6 +12,45 @@ TEAMS = ['Royal Challengers Bangalore', 'Kings XI Punjab', 'Delhi Capitals',
        'Rising Pune Supergiants', 'Punjab Kings', 'Lucknow Super Giants',
        'Gujarat Titans']
 
+STADIUMS = ['M Chinnaswamy Stadium',
+       'Punjab Cricket Association Stadium, Mohali', 'Feroz Shah Kotla',
+       'Wankhede Stadium', 'Eden Gardens', 'Sawai Mansingh Stadium',
+       'Rajiv Gandhi International Stadium, Uppal',
+       'MA Chidambaram Stadium, Chepauk', 'Dr DY Patil Sports Academy',
+       'Newlands', "St George's Park", 'Kingsmead', 'SuperSport Park',
+       'Buffalo Park', 'New Wanderers Stadium', 'De Beers Diamond Oval',
+       'OUTsurance Oval', 'Brabourne Stadium',
+       'Sardar Patel Stadium, Motera', 'Barabati Stadium',
+       'Brabourne Stadium, Mumbai',
+       'Vidarbha Cricket Association Stadium, Jamtha',
+       'Himachal Pradesh Cricket Association Stadium', 'Nehru Stadium',
+       'Holkar Cricket Stadium',
+       'Dr. Y.S. Rajasekhara Reddy ACA-VDCA Cricket Stadium',
+       'Subrata Roy Sahara Stadium',
+       'Maharashtra Cricket Association Stadium',
+       'Shaheed Veer Narayan Singh International Stadium',
+       'JSCA International Stadium Complex', 'Sheikh Zayed Stadium',
+       'Sharjah Cricket Stadium', 'Dubai International Cricket Stadium',
+       'Punjab Cricket Association IS Bindra Stadium, Mohali',
+       'Saurashtra Cricket Association Stadium', 'Green Park',
+       'M.Chinnaswamy Stadium',
+       'Punjab Cricket Association IS Bindra Stadium',
+       'Rajiv Gandhi International Stadium', 'MA Chidambaram Stadium',
+       'Arun Jaitley Stadium', 'MA Chidambaram Stadium, Chepauk, Chennai',
+       'Wankhede Stadium, Mumbai', 'Narendra Modi Stadium, Ahmedabad',
+       'Arun Jaitley Stadium, Delhi', 'Zayed Cricket Stadium, Abu Dhabi',
+       'Dr DY Patil Sports Academy, Mumbai',
+       'Maharashtra Cricket Association Stadium, Pune',
+       'Eden Gardens, Kolkata',
+       'Punjab Cricket Association IS Bindra Stadium, Mohali, Chandigarh',
+       'Bharat Ratna Shri Atal Bihari Vajpayee Ekana Cricket Stadium, Lucknow',
+       'Rajiv Gandhi International Stadium, Uppal, Hyderabad',
+       'M Chinnaswamy Stadium, Bengaluru',
+       'Barsapara Cricket Stadium, Guwahati',
+       'Sawai Mansingh Stadium, Jaipur',
+       'Himachal Pradesh Cricket Association Stadium, Dharamsala',
+       'Maharaja Yadavindra Singh International Cricket Stadium, Mullanpur',
+       'Dr. Y.S. Rajasekhara Reddy ACA-VDCA Cricket Stadium, Visakhapatnam']
 
 PLAYERS = ['A Ashish Reddy', 'A Badoni', 'A Chandila', 'A Chopra',
        'A Choudhary', 'A Dananjaya', 'A Flintoff', 'A Kamboj', 'A Kumble',
