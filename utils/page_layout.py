76cd4d16df7479e82fe270abef3bf6881e7c7b1b
Jai Verma <vermajai2004@gmail.com>
2024-12-25 13:42:11 +0530

    removed custmo navigation, added header and footer

--- utils/page_layout.py
+++ utils/page_layout.py
@@ -1,6 +1,7 @@
 import os
 import streamlit as st
-from utils import data
+import datetime
+from utils.image_markdown import img_to_html
 
 @st.fragment
 def header(title, description, right_image="static/icons/ipl logo.png", left_image="static/icons/IPLAnalytica.png"):
@@ -8,8 +9,8 @@ def header(title, description, right_image="static/icons/ipl logo.png", left_ima
 
     with left:
         if os.path.exists(left_image): 
-            c1, c2, c3 = st.columns([0.2, 0.6, 0.2], gap='small')
-            with c2:
+            c = st.columns([0.2, 0.6, 0.2], gap='small')
+            with c[1]:
                 st.image(left_image, width=180)
         else:
             st.write("Image not found")
@@ -28,13 +29,38 @@ def header(title, description, right_image="static/icons/ipl logo.png", left_ima
 
     with right:
         if os.path.exists(right_image): 
-            c1, c2, c3 = st.columns([0.2, 0.6, 0.2], gap='small')
-            with c2:
+            c = st.columns([0.2, 0.6, 0.2], gap='small')
+            with c[1]:
                 st.image(right_image)
         else:
             st.write("Image not found")
 
 
+import datetime
+
 @st.fragment
 def footer():
-    pass
+    current_year = datetime.datetime.now().year
+    st.divider()
+    cols = st.columns([1, 1, 1], gap='medium')
+    
+    with cols[1]:
+        st.write(f"© {current_year} IPL Analytica")
+        st.write("[Contact](mailto:vermajai2004@gmail.com)")
+
+    with cols[0]:
+        center_cols = st.columns([1, 1, 1], gap="small")
+        
+        with center_cols[0]:
+            st.image("static/icons/linkedin.png", width=50)
+            st.write("[LinkedIn](https://linkedin.com/in/jai-verma)")
+        
+        with center_cols[1]:
+            st.image("static/icons/github.png", width=50)
+            st.write("[GitHub](https://github.com/Jai-Verma-04)")
+
+    with cols[2]:
+        st.write("[Portfolio - Coming Soon]()")
+            
+
+
