f4385c314f8c6e16182738debbcebcd3b4ae2c7b
Jai Verma <vermajai2004@gmail.com>
2024-12-26 22:58:08 +0530

    [completed] Home page

--- HOME.py
+++ HOME.py
@@ -1,11 +1,62 @@
 import streamlit as st
-# from utils.navigation import custom_sidebar_navigation
+from utils.page_layout import header, footer
 
-# custom_sidebar_navigation("Home")
-st.write("Welcome to IPL Web App Project")
+st.set_page_config(page_icon="🏠", page_title="Welcome to IPLAnalytica", layout="wide", initial_sidebar_state='collapsed')
 
-st.info("""The Home Page is currently under construction. 
-        Checkout Season Wise Stats and Generate Scorecard page from the sidebar
-        """)
+header(title="Welcome to IPLAnalytica", description="""
+1. Navigate through pages to uncover in-depth insights into IPL's history and performances.<br>
+2. Explore scorecards, team rivalries, player stats, venue performances, and season highlights.<br>
+3. Each page offers unique insights, from head-to-head stats to teams and players over the seasons.
+""")
 
-st.info("Checkout the repo at: https://github.com/Jai-Verma-04/IPLAnalytica", )
+st.markdown("""
+## Welcome to IPLAnalytica Web App!
+* This project is a data analysis and visualization tool for exploring statistics related to the Indian Premier League (IPL) Cricket data. 
+
+* The project provides insights into player and team performances, match statistics, and an added functionality to generate scorecard of a particular match.
+
+* The project is built upon the [streamlit](https://streamlit.io/) app framework to create a web app and help users better understand the insights gained from the data analysis of the [IPL Data](https://www.kaggle.com/datasets/patrickb1912/ipl-complete-dataset-20082020) using Python's Pandas library.
+""")
+
+st.markdown("""
+## Lessons Learned
+* The IPL Analytics Project provided valuable experience in **cleaning, filtering, and extracting insights** from large datasets using Pandas. 
+
+* The implementation involved applying advanced **data manipulation techniques, including filtering, grouping, and aggregation**, to derive meaningful patterns and trends from the data. 
+
+* The project also demonstrated the effective use of **Streamlit for deploying an interactive web application**, enabling seamless presentation of insights to users. 
+""")
+
+st.markdown("""
+## Running locally from your system
+
+To run the project on your local machine run the following commands in your **command prompt** one by one.
+
+1. Clone the github repository:
+
+```bash
+  git clone https://github.com/Jai-Verma-04/IPLAnalytica.git
+```
+2. Change current directory to IPLAnalytica folder
+```bash
+  cd path/to/folder/IPLAnalytica
+```
+3. Create a virtual environment (optional):
+```bash
+python -m venv /path/to/folder/IPLAnalytica
+```
+3. Install the required dependencies using:
+```bash
+pip install requirements.txt
+```
+4. The run the app using streamlit command:
+```bash
+streamlit run HOME.py
+```
+The app will open in your web browser, where you can navigate through the various pages.
+
+""")
+
+st.header("Open a github issue [here](https://github.com/Jai-Verma-04/IPLAnalytica/issues)")
+
+footer()
